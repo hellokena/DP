88aae7257eb28948f634f35c1af0ca809e58263e
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-18 21:38:33 +0900

    Update BOJ#1463_1로 만들기.py

--- BOJ#1463_1로 만들기.py
+++ BOJ#1463_1로 만들기.py
@@ -6,7 +6,10 @@ n = int(sys.stdin.readline().rstrip())
 result = [0]*(n+1) # 알기 쉽게 하기 위해서 첫 시작 인덱스를 1로 둔다
 result[1] = 0
 for i in range(2, n+1):
-    result[i] = result[i-1]+1
+    # N은 2,3의 배수가 아닐 경우를 대비하여 우선 1을 더하는 방식으로 
+    # for문을 통해 값을 초기화 해 준다.
+    result[i] = result[i-1]+1 # 전 인덱스 값에서의 +1으로 초기화 해준다.
+    # 2와 3은 둘다 해줘야함 elif문 쓰면 안됨
     if i%2==0: result[i] = min(result[i], result[i//2]+1)
     if i%3==0: result[i] = min(result[i], result[i//3]+1)
 print(result[n])
